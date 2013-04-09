4f3154748a0da9ba3f735bec41f0cc01e6ca7040
Haozi Leung <haozileung.cn@gmail.com>
2013-04-09 16:40:50 +0800

    添加GWT测试框架 重新布局资源文件
    Signed-off-by: Haozi Leung <haozileung.cn@gmail.com>

---
+++ src/test/java/com/haozileung/scau/client/home/View/SportDataViewTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.haozileung.scau.client.home.View;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * @author lianghaopeng
+ * 
+ */
+public class SportDataViewTest extends GWTTestCase {
+
+	@Override
+	public String getModuleName() {
+		// TODO Auto-generated method stub
+		return "com.haozileung.scau.CowHealth";
+	}
+
+	/**
+	 * Add as many tests as you like.
+	 */
+	public void testSimple() {
+		assertTrue(true);
+	}
+
+}
